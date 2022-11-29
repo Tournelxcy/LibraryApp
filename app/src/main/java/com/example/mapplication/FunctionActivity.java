027841bd940d0a475d44575a6f67bb11e71bb208
Tournelxcy <1784277178@qq.com>
2022-11-29 19:29:53 +0800

    Change the APP name and icon

--- app/src/main/java/com/example/mapplication/FunctionActivity.java
+++ app/src/main/java/com/example/mapplication/FunctionActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FountionActivity extends AppCompatActivity {
+public class FunctionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fountion);
+        setContentView(R.layout.activity_function);
     }
 }
