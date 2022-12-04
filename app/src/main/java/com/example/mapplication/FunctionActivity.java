33951a226fcd0e09dc0d7d21a53a53b9a94586b4
Tournelxcy <1784277178@qq.com>
2022-12-04 15:29:18 +0800

    Fixed a bug that caused the jump to flash back

--- app/src/main/java/com/example/mapplication/FunctionActivity.java
+++ app/src/main/java/com/example/mapplication/FunctionActivity.java
@@ -85,7 +85,7 @@ public class FunctionActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_function);
         //找到各个按钮
         imgbtn=(ImageButton)findViewById(R.id.title_imgbtn);
         imgbtn2=(ImageButton) findViewById(R.id.title_imgbtn2);
